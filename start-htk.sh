eb3e3761c3557a5a7a029b164106a1278fdcfdfe
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2015-01-03 15:43:04 +0800

    Initial values

--- start-htk.sh
+++ start-htk.sh
@@ -96,7 +96,7 @@ generate_hmmdefs() {
 generate_macros() {
   echo "~o" > Models/hmm0/macros
   echo "<STREAMINFO> 1 39" >> Models/hmm0/macros
-  echo "<VECSIZE> 39<NULLD><MFCC_D_A_0><DIAGC>" >> Models/hmm0/macros
+  echo "<VECSIZE> 39<NULLD><MFCC_E_D_A><DIAGC>" >> Models/hmm0/macros
   cat Models/hmm0/vFloors >> Models/hmm0/macros
 }
 
